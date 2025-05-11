ecf854b14b737bfbe001e8ee51cedc482b87f789
Arjundevjha <111162691+Arjundevjha@users.noreply.github.com>
2025-05-11 07:49:25 +0000

    Hosting 3rd solution

--- pages/api/process.js
+++ pages/api/process.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   const { prompt } = req.body;
-  const backendUrl = 'https://sl-chatbot-1.onrender.com/process';
+  const backendUrl = process.env.BACKEND_URL || 'http://127.0.0.1:5000/process';
 
   try {
     const response = await fetch(backendUrl, {
