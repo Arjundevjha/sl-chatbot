be9e464d57f4457bfd6e48f0830a2e03e18f8037
Arjundevjha <111162691+Arjundevjha@users.noreply.github.com>
2025-05-11 05:34:30 +0000

    Backend url 2nd change

--- pages/api/process.js
+++ pages/api/process.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   const { prompt } = req.body;
-  const backendUrl = 'http://0.0.0.0:5000/api/process';
+  const backendUrl = 'https://sl-chatbot-b09r.onrender.com/process';
 
   try {
     const response = await fetch(backendUrl, {
