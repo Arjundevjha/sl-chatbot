c1af389b1e1278ba032d630e0b2a87fccc834593
Arjundevjha <111162691+Arjundevjha@users.noreply.github.com>
2025-05-11 03:43:05 +0000

    backend url change

--- pages/api/process.js
+++ pages/api/process.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   const { prompt } = req.body;
-  const backendUrl = 'http://0.0.0.0:5000/process';
+  const backendUrl = 'http://0.0.0.0:5000/api/process';
 
   try {
     const response = await fetch(backendUrl, {
